a024d7b7baf16a4a792708c97e22cbedce7a1948
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-07-19 22:01:06 +0300

    swap Outfit font to Lexend Deca thin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Outfit } from "next/font/google";
+import {  Outfit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
