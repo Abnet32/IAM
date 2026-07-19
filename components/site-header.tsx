0b4b7aac885fe53bc5c29a2a0128c4a48694209b
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-07-19 22:00:53 +0300

    add theme toggle to dashboard header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,9 +1,10 @@
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { ModeToggle } from "@/components/theme-toggle";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2  transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
@@ -11,8 +12,10 @@ export function SiteHeader() {
           className="mx-2 h-4 data-vertical:self-auto"
         />
         <h1 className="text-base font-medium">Documents</h1>
+        <div className="ml-auto">
+          <ModeToggle />
+        </div>
       </div>
-      di
     </header>
-  )
+  );
 }
